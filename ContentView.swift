640ac9a3b59b3e8f7a5ff61860fac3d6f28a3a59
Nelson Taruc <nelson.taruc@lextech.com>
2025-05-30 16:38:34 -0500

    Test Commit
    This is a test commit using Working Copy

---
+++ ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("This is a test of how to use Working Copy.")
+                        Text("Need pro version of Working Copy")
+            Text("Need Swift Playground")
+        }
+    }
+}
